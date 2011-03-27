b5f1ebe68297b5f7c9c108c41d202541bbc61a2a
bmaher <briancmaher@gmail.com>
2011-03-27 02:45:50 +0100

    Can now destroy users (as admins).

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20110326003921
+# Schema version: 20110327010344
 #
 # Table name: users
 #
@@ -10,6 +10,7 @@
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean
 #
 
 class User < ActiveRecord::Base
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,8 @@ describe User do
   before(:each) do
     @attr = { :name => "Example User",
               :email => "user@example.com",
-              :password => "foobar",
-              :password_confirmation => "foobar" }
+              :password => "password",
+              :password_confirmation => "password" }
   end
 
   it "should create a new instance given valid attributes" do
@@ -71,6 +71,7 @@ describe User do
     it "should have a password confirmation attribute" do
       @user.should respond_to(:password_confirmation)
     end
+    
   end
   
   describe "password validations" do
@@ -96,6 +97,7 @@ describe User do
       User.new(@attr.merge(:password => long, :password_confirmation => long)).
         should_not be_valid
     end
+    
   end
   
   describe "password encryption" do
@@ -129,6 +131,7 @@ describe User do
       it "should return false if the passwords don't match" do
         @user.has_password?("invalid").should be_false
       end
+      
     end
     
     describe "authenticate method" do
@@ -148,7 +151,30 @@ describe User do
       it "should return the user on email/password match" do
         User.authenticate(@attr[:email], @attr[:password]).should == @user
       end
+      
     end
+    
+  end
+  
+  describe "admin attribute" do
+    
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it "should respond to admin" do
+      @user.should respond_to(:admin)
+    end
+    
+    it "should not be an admin by default" do
+      @user.should_not be_admin
+    end
+    
+    it "should be convertible to an admin" do
+      @user.toggle!(:admin)
+      @user.should be_admin
+    end
+    
   end
     
 end
